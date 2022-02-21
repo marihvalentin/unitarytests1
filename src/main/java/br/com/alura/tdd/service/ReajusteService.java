ee18df529612018b667366d1b8e97b2de0ae537b
Mariana Valentim <marihvalentin_2@hotmail.com>
2022-02-21 20:54:43 -0300

    Seguindo a prática de Testes TDD, foi realizada a implementação de testes para reajuste salarial considerando o desempenho e após foram implementadas a classe Service correspondente e a Enum de Desempenho

---
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,26 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+import java.math.BigDecimal;
+
+public class ReajusteService {
+
+    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+        if (desempenho == Desempenho.A_DESEJAR) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            funcionario.reajustarSalario(reajuste);
+        }
+
+        if (desempenho == Desempenho.BOM) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
+            funcionario.reajustarSalario(reajuste);
+        }
+
+        if(desempenho == Desempenho.OTIMO) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
+            funcionario.reajustarSalario(reajuste);
+        }
+    }
+}
