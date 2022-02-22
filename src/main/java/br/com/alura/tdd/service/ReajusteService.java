3fa9e4703e1d88ea5ac92e0173f75ce81c8b254a
Mariana Valentim <marihvalentin_2@hotmail.com>
2022-02-21 22:47:29 -0300

    Realizada refactoring na implementação para simplificar o código e possíveis manutenções, seguindo princípio TDD

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,19 +8,8 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if(desempenho == Desempenho.OTIMO) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        }
+       BigDecimal percentual = desempenho.percentualReajuste();
+       BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+       funcionario.reajustarSalario(reajuste);
     }
 }
